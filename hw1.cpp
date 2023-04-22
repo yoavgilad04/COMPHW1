2d4eb537a6f9fb6ca7256e271725572ba5760f3d
yoavgilad04 <yoavgilad1995g@gmail.com>
2023-04-22 20:49:42 +0300

    f Scanner

--- hw1.cpp
+++ hw1.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "tokens.hpp"
-
+#include <iostream>
 int main()
 {
 	int token;
 	while ((token = yylex())) {
-        printf(token);
+        std::cout<< token << "I'm here";
         exit(0);
 	}
 	return 0;

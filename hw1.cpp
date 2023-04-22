64d0acba6501bf9f3246745af8e7c88055c5cc9f
yoavgilad04 <yoavgilad1995g@gmail.com>
2023-04-22 18:14:35 +0300

    Adding files

---
+++ hw1.cpp
@@ -0,0 +1,12 @@
+#include "tokens.hpp"
+#include <iostream>
+
+int main()
+{
+	int token;
+    std::cout << "hello world";
+//	while ((token = yylex())) {
+//	  // Your code here
+//	}
+//	return 0;
+}
